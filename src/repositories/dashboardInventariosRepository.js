f3a4698482f6034c613cb994fc63ab168ad3152d
Oscar Rodriguez <118637313+cetusmx@users.noreply.github.com>
2026-07-10 00:53:22 -0600

    3.09

--- src/repositories/dashboardInventariosRepository.js
+++ src/repositories/dashboardInventariosRepository.js
@@ -7,6 +7,11 @@ const db = require('../db'); // Conexión a Empresa 2
 const obtenerMovimientosYClasificacion = async (refer, productos) => {
     const dbResults = [];
     const chunkSize = 200;
+
+    console.log("refer: ", refer);
+    console.log("- - - - - - - - - -");
+    console.log("productos: ", productos);
+    
     
     // Limpieza de espacios para evitar fallos de strings binarios en Firebird
     const cleanedClaves = productos.map(p => String(p).trim());
